c538bc66b08f00ea1297fc521d08fba1108ea45f
SSingh <ssingh@kenmarkitan.com>
2024-06-12 00:33:40 +0530

    Initial commit

---
+++ frontend/src/components/Diet.jsx
@@ -0,0 +1,137 @@
+import { useEffect, useState, useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
+import Header from './Header';
+import './diet.css';
+import { FaTimes } from 'react-icons/fa';
+
+export default function Diet() {
+    const loggedData = useContext(UserContext);
+    const [items, setItems] = useState([]);
+    const [date, setDate] = useState(new Date());
+    const [total, setTotal] = useState({
+        totalCaloreis: 0,
+        totalProtein: 0,
+        totalCarbs: 0,
+        totalFats: 0,
+        totalFiber: 0
+    });
+
+    useEffect(() => {
+        // Fetch data only if date is selected
+        if (date !== null) {
+            // Convert the date to IST
+            const istDate = new Date(date.toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }));
+            fetch(`http://localhost:8000/track/${loggedData.loggedUser.userid}/${istDate.getMonth() + 1}-${istDate.getDate()}-${istDate.getFullYear()}`, {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${loggedData.loggedUser.token}`
+                }
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setItems(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        } else {
+            setItems([]);
+            setTotal({
+                totalCaloreis: 0,
+                totalProtein: 0,
+                totalCarbs: 0,
+                totalFats: 0,
+                totalFiber: 0
+            });
+        }
+    }, [date, loggedData.loggedUser.userid, loggedData.loggedUser.token]);
+
+    useEffect(() => {
+        calculateTotal();
+    }, [items]);
+
+    function calculateTotal() {
+        let totalCopy = {
+            totalCaloreis: 0,
+            totalProtein: 0,
+            totalCarbs: 0,
+            totalFats: 0,
+            totalFiber: 0
+        };
+
+        items.forEach((item) => {
+            totalCopy.totalCaloreis += item.details.calories;
+            totalCopy.totalProtein += item.details.protein;
+            totalCopy.totalCarbs += item.details.carbohydrates;
+            totalCopy.totalFats += item.details.fat;
+            totalCopy.totalFiber += item.details.fiber;
+        });
+
+        setTotal(totalCopy);
+    }
+
+    function deleteItem(foodId) {
+        fetch(`http://localhost:8000/track/${loggedData.loggedUser.userid}/${foodId}`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Bearer ${loggedData.loggedUser.token}`
+            }
+        })
+        .then((response) => {
+            if (response.ok) {
+                // Remove the item from the UI
+                setItems(items.filter(item => item._id !== foodId));
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
+    const IST_OFFSET = 5.5; // IST is UTC+5.5
+
+    const toIST = (date) => {
+        const offsetInMillis = date.getTimezoneOffset() * 60 * 1000; // Offset in milliseconds
+        const istInMillis = offsetInMillis + (IST_OFFSET * 60 * 60 * 1000); // IST offset in milliseconds
+        return new Date(date.getTime() + istInMillis); // Add IST offset to get IST time
+      };
+      
+      const toDateString = (date) => {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // Add leading zero if needed
+        const day = String(date.getDate()).padStart(2, '0'); // Add leading zero if needed
+        return `${year}-${month}-${day}`;
+      };
+    return (
+        <section className="container diet-container back" style={{ backgroundImage: "url('https://img.freepik.com/free-photo/burger-french-fries-plate-with-copy-space_23-2148235009.jpg?w=740&t=st=1718128904~exp=1718129504~hmac=527e618c8c22a6c1c419929de15931c70fbcc4aac627fc18b9d5400cdbe5fc46')" }}>
+            
+            <Header />
+            <input 
+    type="date" 
+    value={date ? toDateString(toIST(date)) : ''}
+    onChange={(event) => {
+        const newDate = event.target.value ? new Date(event.target.value) : null;
+        console.log(newDate);
+        setDate(newDate ? toIST(newDate) : null);
+    }}
+
+/>
+
+            {items.map((item) => {
+                return (
+                    <div className="item" key={item._id}>
+                        <h3>{item.foodId.name} ( {item.details.calories} Kcal for {item.quantity}g )</h3>
+                        <p>Protein {item.details.protein.toFixed(4)}g, Carbs {item.details.carbohydrates.toFixed(4)}g, Fats {item.details.fat.toFixed(4)}g, Fiber {item.details.fiber.toFixed(4)}g</p>
+                        <FaTimes className="delete-icon" onClick={() => deleteItem(item._id)} />
+                    </div>
+                );
+            })}
+
+            <div className="item head">
+                <h3 className="heading"> Total - {total.totalCaloreis.toFixed(4)} Kcal </h3>
+                <p>Protein {total.totalProtein.toFixed(4)}g, Carbs {total.totalCarbs.toFixed(4)}g, Fats {total.totalFats.toFixed(4)}g, Fiber {total.totalFiber.toFixed(4)}g</p>
+            </div>
+        </section>
+    );
+}
---
+++ frontend/src/components/Track.jsx
@@ -0,0 +1,112 @@
+import { UserContext } from "../contexts/UserContext";
+import { useContext, useState, useEffect } from "react";
+import Food from "./Food";
+import Header from './Header';
+import './styles.css';
+
+export default function Track() {
+    const loggedData = useContext(UserContext);
+
+    const [foodItems, setFoodItems] = useState([]);
+    const [food, setFood] = useState(null);
+    const [searchValue, setSearchValue] = useState("");
+    const [searchClicked, setSearchClicked] = useState(false); 
+
+    useEffect(() => {
+        fetchAllFoodItems();
+    }, []);
+
+    const fetchAllFoodItems = () => {
+        fetch("http://localhost:8000/foods", {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${loggedData.loggedUser.token}`
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            if (data.message === undefined) {
+                setFoodItems(data);
+            } else {
+                setFoodItems([]);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    };
+
+    const handleSearchInputClick = () => {
+        setSearchClicked(true); 
+    };
+
+    const handleSearchInputChange = (event) => {
+        setSearchValue(event.target.value);
+        if (event.target.value.length !== 0) {
+            fetch(`http://localhost:8000/foods/${event.target.value}`, {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${loggedData.loggedUser.token}`
+                }
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                if (data.message === undefined) {
+                    setFoodItems(data);
+                } else {
+                    setFoodItems([]);
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        } else {
+            setFoodItems([]);
+        }
+    };
+
+    const handleFoodItemClick = (item) => {
+        setFood(item);
+        setSearchClicked(false);  
+        setSearchValue(item.name); 
+    };
+
+    return (
+        <>
+            {/* <section className="container track-container"> */}
+            <section className="container track-container back" style={{ backgroundImage: "url('https://img.freepik.com/free-photo/burger-french-fries-plate-with-copy-space_23-2148235009.jpg?w=740&t=st=1718128904~exp=1718129504~hmac=527e618c8c22a6c1c419929de15931c70fbcc4aac627fc18b9d5400cdbe5fc46')" }}>
+                <Header />
+                <div className="search">
+                    <input 
+                        className="search-inp" 
+                        onClick={handleSearchInputClick}
+                        onChange={handleSearchInputChange}
+                        value={searchValue}
+                        type="search" 
+                        placeholder="Search Food Item"
+                    />
+                    {searchClicked && ( 
+                        <div className="search-results">
+                            {foodItems.map((item) => (
+                                <p 
+                                    className="item" 
+                                    onClick={() => handleFoodItemClick(item)}
+                                    key={item._id}
+                                >
+                                    {item.name}
+                                </p>
+                            ))}
+                        </div>
+                    )}
+                </div>
+                {food !== null && (
+                    <div className="food-container">
+                        <Food food={food} />
+                    </div>
+                )}
+            </section>
+        </>
+    );
+}
